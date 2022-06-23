638ccc9735ec81ee12db294313e1413e22012e2a
Silas Odhiambo <silasodhiambokibu@gmail.com>
2022-06-23 12:58:51 +0300

    Commit

---
+++ Lost/app/src/main/java/com/example/lost/PostAdapter.java
@@ -0,0 +1,50 @@
+package com.example.lost;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
+
+import java.util.List;
+
+
+public class PostAdapter extends ArrayAdapter<Post> {
+
+    // Declare variables
+
+    private FragmentActivity context;
+
+    private List<Post> postList;
+
+    private TextView textViewTitle, textViewDescription;
+
+    public PostAdapter(FragmentActivity context, List<Post> postList){
+        super(context, R.layout.card_post,postList);
+        this.context = context;
+        this.postList = postList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.card_post, null, true);
+
+        // Initialize
+        textViewTitle = (TextView) view.findViewById(R.id.textViewTitle);
+        textViewDescription = (TextView)view.findViewById(R.id.textViewDescription);
+
+        textViewDescription.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        Post post = postList.get(position);
+
+        // Set the title and description to textView
+        textViewTitle.setText(post.getTitle());
+        textViewDescription.setText(post.getDescription());
+
+        return view;
+    }
+}
---
+++ Lost/app/src/main/java/com/example/lost/Upload.java
@@ -0,0 +1,24 @@
+package com.example.lost;
+
+// Upload class to hold image url
+
+public class Upload {
+
+    private String imageUrl;
+
+    public Upload() {
+
+    }
+
+    public Upload(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
